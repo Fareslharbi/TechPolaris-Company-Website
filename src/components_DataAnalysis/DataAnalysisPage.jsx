52574d3ab76004617c92ab325ccebcd9d6e58608
Fares Alharbi <engfares@Faress-MacBook-Pro.local>
2023-07-15 05:27:38 +0300

    update services

--- src/components_DataAnalysis/DataAnalysisPage.jsx
+++ src/components_DataAnalysis/DataAnalysisPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components_Home_Page/1_header/Header";
+import Footer from "../components_Home_Page/10_Footer/Footer";
 const DataAnalysisPage = () => {
   return (
     <div>
       <Header />
       <h1 className="mt-5 pt-5">DataAnalysisPage</h1>
+      <h1 className="mt-5 pt-5">DataAnalysisPage</h1>
+      <h1 className="mt-5 pt-5">DataAnalysisPage</h1>
+      <Footer />
     </div>
   );
 };
--- src/components_News/NewsPage.jsx
+++ src/components_News/NewsPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components_Home_Page/1_header/Header";
+import Footer from "../components_Home_Page/10_Footer/Footer";
 const NewsPage = () => {
   return (
     <div>
       <Header />
       <h1 className="mt-5 pt-5">NewsPage</h1>
+      <h1 className="mt-5 pt-5">NewsPage</h1>
+      <h1 className="mt-5 pt-5">NewsPage</h1>
+      <Footer />
     </div>
   );
 };
