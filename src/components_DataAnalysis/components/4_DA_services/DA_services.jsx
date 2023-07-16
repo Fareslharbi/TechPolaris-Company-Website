0c58bd35ad298a2d8ad06273257e9a23420286e6
Fares Alharbi <engfares@Faress-MacBook-Pro.local>
2023-07-16 07:54:52 +0300

    list

--- src/components_DataAnalysis/components/4_DA_services/DA_services.jsx
+++ src/components_DataAnalysis/components/4_DA_services/DA_services.jsx
@@ -5,6 +5,32 @@ import img1 from "../../assets/Data-as-a-Service.svg.png";
 import img2 from "../../assets/Data-Analytics-Implementation.svg.png";
 import img3 from "../../assets/Data-Analytics-support-and-Evolution.svg.png";
 const DA_services = () => {
+  const data = [
+    {
+      id: 0,
+      title: "Data Analytics Consulting",
+      desc: "We offer data analytics consulting services to all our clients so that they can benefit from the best solutions we have. After we understand their business goal, we help them find the optimum data analytics solution for your company.",
+      image: img,
+    },
+    {
+      id: 1,
+      title: "Data Analytics As A Service",
+      desc: "With data analytics as a service, you can derive useful insights out of the raw data. This can help you avoid the time as well as cost of developing a full-scale data analytics solution",
+      image: img1,
+    },
+    {
+      id: 2,
+      title: "Data Analytics Implementation",
+      desc: "With DataToBiz, you can get the best data analytics implementation services. We help you find the right kind of analytics solution for your company, ensuring that you get the best results.",
+      image: img2,
+    },
+    {
+      id: 3,
+      title: "Data Analytics Support & Evolution",
+      desc: "Between data analytics consulting services and implementation services, we also are renowned for our top-class support facility. With DataToBiz, you get a full-tuned support team to help you in case of any issue.",
+      image: img3,
+    },
+  ];
   return (
     <div className="DA_services_container">
       <h1>What Can We Do For You?</h1>
@@ -17,61 +43,21 @@ const DA_services = () => {
         ranges from consulting to implementation of the right data analytics
         solution for your company.
       </p>
+
       <div className="card-group">
-        <div className="card border rounded bg-light">
-          <div className="card-body">
-            <div className="img-fluid mb-4">
-              <img src={img} alt="DA sec_logo" />
+        {data.map(({ id, title, desc, image }) => {
+          return (
+            <div id={id} className="card border rounded bg-light">
+              <div className="card-body">
+                <div className="img-fluid mb-4">
+                  <img src={image} alt="DA sec_logo" />
+                </div>
+                <h3>{title}</h3>
+                <p>{desc}</p>
+              </div>
             </div>
-            <h3>Data Analytics Consulting</h3>
-            <p>
-              We offer data analytics consulting services to all our clients so
-              that they can benefit from the best solutions we have. After we
-              understand their business goal, we help them find the optimum data
-              analytics solution for your company.
-            </p>
-          </div>
-        </div>
-        <div className="card border rounded bg-light">
-          <div className="card-body">
-            <div className="img-fluid mb-4">
-              <img src={img1} alt="DA sec_logo" />
-            </div>
-            <h3>Data Analytics As A Service</h3>
-            <p>
-              With data analytics as a service, you can derive useful insights
-              out of the raw data. This can help you avoid the time as well as
-              cost of developing a full-scale data analytics solution
-            </p>
-          </div>
-        </div>
-        <div className="card border rounded bg-light">
-          <div className="card-body">
-            <div className="img-fluid mb-4">
-              <img src={img2} alt="DA sec_logo" />
-            </div>
-            <h3>Data Analytics Implementation</h3>
-            <p>
-              With DataToBiz, you can get the best data analytics implementation
-              services. We help you find the right kind of analytics solution
-              for your company, ensuring that you get the best results.
-            </p>
-          </div>
-        </div>
-        <div className="card border rounded bg-light">
-          <div className="card-body">
-            <div className="img-fluid mb-4">
-              <img src={img3} alt="DA sec_logo" />
-            </div>
-            <h3>Data Analytics Support & Evolution</h3>
-            <p>
-              Between data analytics consulting services and implementation
-              services, we also are renowned for our top-class support facility.
-              With DataToBiz, you get a full-tuned support team to help you in
-              case of any issue.
-            </p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
